ca136e51450af57c7a2c2f8cd0a7272f18bbe827
Sergey <Dgoli.region37@yandex.ru>
2022-12-26 22:07:59 +0300

    Example_17

---
+++ Example_13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача №13: 
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Например:
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+//int index = 0;
+int counter = 1;
+int newnumber = 1;
+//Console.WriteLine(newnumber);
+
+while (newnumber > 0)
+{
+    newnumber = number / 10;
+    counter = counter + 1;
+    //Console.WriteLine(counter);
+   
+}
+Console.WriteLine(counter);
---
+++ Example_17/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача N17/
+Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X=/ 0 и Y =/ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+    }
+int getQuarterNumber(int x, int y)
+{
+    int quarter = 0;
+    if (x > 0 && y > 0)
+    {
+        quarter = 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        quarter = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        quarter = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        quarter = 4;
+    }
+    return quarter;
+}
+
+int userX = getUserValue ("Введите X");
+int userY = getUserValue ("Введите Y");
+int quarter = getQuarterNumber(userX, userY);
+if (quarter > 0)
+{
+    Console.WriteLine($"Точка [{userX}, {userY}] находится в {quarter} четверти");
+}
+    else
+{
+     Console.WriteLine($"Точка [{userX}, {userY}] находится на оси координат");
+}
+
+   
+
